e705074d8cebc5e1d377948c552d7111314bba1a
Khalem <33363455+Khalem@users.noreply.github.com>
2019-09-13 19:17:05 +0100

    Changed App to class and added state
    Added states to render the joke to <p> each time the button is clicked.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,41 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends React.Component {
+  joke = null;
+
+  constructor() {
+    super();
+
+    this.state = {
+      joke: null
+    };
+    this.onTellJoke = this.onTellJoke.bind(this);
+  }
+
+  onTellJoke() {
+    fetch("https://icanhazdadjoke.com/", {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ joke: json.joke });
+      });
+  }
+
+  render() {
+    console.log("----- RENDER -----");
+
+    return (
+      <div>
+        <button onClick={this.onTellJoke}>Tell Me a Joke</button>
+        <p>{this.state.joke}</p>
+      </div>
+    );
+  }
 }
 
 const rootElement = document.getElementById("root");
