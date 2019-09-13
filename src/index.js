e9627ae0c7303dc6d87a6748e36853087670ffcb
Khalem <33363455+Khalem@users.noreply.github.com>
2019-09-13 19:06:30 +0100

    Created JSX button
    Created a JSX button that prints a random joke object to console using the 'icanhazdadjoke' API.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+  const onTellJoke = () => {
+    fetch("https://icanhazdadjoke.com/", {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(json => console.log(json));
+  };
+
+  return <button onClick={onTellJoke}>Tell Me a Joke</button>;
 }
 
 const rootElement = document.getElementById("root");
